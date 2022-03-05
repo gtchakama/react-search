4ebf1395978159ad82d6d777e8db75e2871bd2ae
George_Chakama <gtchakama@gmail.com>
2022-03-05 16:16:24 +0200

    Create blogData.js

---
+++ src/data/blogData.js
@@ -0,0 +1,39 @@
+const blogData = [
+    {
+      id: 1,
+      imgPath: "/assets/img/1.png",
+      article: "Using JS like an Expect 💪🏼",
+      link: "https://www.chakama.co.zw",
+      description: "Be productive in the way you work with JavaScript and practice ``DRY`` ( Don't Repeat Yourself).",
+    },
+    {
+      id: 2,
+      imgPath: "/assets/img/2.png",
+      article: "What is This ? 🥱",
+      link: "https://www.chakama.co.zw",
+      description: "The `this` keyword is a reference variable that refers to the current object.",
+    },
+    {
+      id: 3,
+      imgPath: "/assets/img/3.png",
+      article: "== or === 😟",
+      link: "https://www.chakama.co.zw",
+      description: "JavaScript has two almost identical ways to test equality. You can test equality with `==` or `===`. This post will explain the difference between these two operators and how to decide when to use one over the other.",
+    },
+    {
+      id: 4,
+      imgPath: "/assets/img/4.png",
+      article: "What are Cookies 🍪",
+      link: "https://www.chakama.co.zw",
+      description: "A cookie can only contain so much text but apart from its size the possibilities are endless what exactly is saved to a cookie is up to the creator of the website you're visiting now.",
+    },
+    {
+      id: 5,
+      imgPath: "/assets/img/5.png",
+      article: "3 Ways my Code got Better 😉",
+      link: "https://www.chakama.co.zw",
+      description: "Growing up I've always been a curious fellow. I still remember when my Dad first beat me up after breaking down our old TV because I just wanted to see how it worked 🤦🏽‍♂️.",
+    },
+  ];
+  
+  export default blogData;
